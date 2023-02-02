32f8db9f209287b813e353b5c3058293f6c08892
Alvin Ly <alvinly12@yahoo.com>
2023-02-01 23:29:02 -0800

    Added extra stuff to outline

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { AlertController } from '@ionic/angular';
+
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -7,6 +10,37 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+
+  async enter_info()
+  {
+    // Haven't looked into storing this information yet
+
+    const alert = await this.alertController.create({
+      header: 'Please enter your info',
+      buttons: ['OK'],
+      inputs: [
+        {
+          placeholder: 'Name',
+        },
+        {
+          type: 'number',
+          placeholder: 'Weight',
+        },
+        {
+          type: 'number',
+          placeholder: 'Age',
+          min: 1,
+          max: 100,
+        },
+        {
+          placeholder: "Male / Female"
+        },
+      ],
+    });
+
+    await alert.present();
+
+  }
 }
