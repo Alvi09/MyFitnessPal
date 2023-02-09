b1cca701d7025a7a55c452e91ef04570f42299da
Alan Yi <yi.alan10@gmail.com>
2023-02-08 16:23:45 -0800

    implemented recommended macros

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 import { AlertController } from '@ionic/angular';
 
-
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -10,7 +9,11 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab3Page {
   userInfo: any[] = [];
-
+  informationEntered = true
+  calories = 0
+  protein = 0
+  carbs = 0
+  fat = 0
   constructor(private alertController: AlertController) {}
 
 
@@ -29,43 +32,61 @@ export class Tab3Page {
             const age = alertData.age;
             const height = alertData.height;
             const weight = alertData.weight;
-            const gender = alertData.gender;
+            const gender = alertData.gender;   
+            const goal = alertData.goal        
+            this.userInfo[0] = ({name: name, age: age, height: height, weight: weight, gender: gender, goal: goal});
+            if (gender == 'male')
+              this.calories = Math.floor((88.362 + (13.397 * this.userInfo[0].weight) + (4.799 * this.userInfo[0].height) - (5.677 * this.userInfo[0].age)) * 1.55)
+            else
+              this.calories = Math.floor((447.593 + (9.247 * this.userInfo[0].weight) + (3.098 * this.userInfo[0].height) - (4.330 * this.userInfo[0].age)) * 1.55)
+            if (goal == 'loss')
+              this.calories -= 500
+            else if (goal == 'gain')
+              this.calories += 500
 
-            this.userInfo[0] = ({name: name, age: age, height: height, weight: weight, gender: gender});
+            
+            this.protein = Math.floor(this.calories * .07)
+            this.carbs = Math.floor(this.calories * .05)
+            this.fat = Math.floor(this.calories * .03333)
+
             console.log(this.userInfo);
+            this.informationEntered = false;
           }
         }],
-      inputs: [
-        {
-          name: "name",
-          placeholder: 'Name',
-        },
-        {
-          name: "age",
-          type: 'number',
-          placeholder: 'Age',
-          min: 1,
-          max: 100,
-        },
-        {
-          name: "height",
-          placeholder: "Height"
-        },
-        {
-          name: "weight",
-          type: 'number',
-          placeholder: 'Weight',
-          min: 1,
-          max: 1000,
-        },
-        {
-          name: "gender",
-          placeholder: "Gender"
-        },
-      ],
-    });
-
-    await alert.present();
-
+        inputs: [
+          {
+            name: "name",
+            placeholder: 'Name',
+          },
+          {
+            name: "age",
+            type: 'number',
+            placeholder: 'Age',
+            min: 1,
+            max: 100,
+          },
+          {
+            name: "height",
+            placeholder: "Height (cm)"
+          },
+          {
+            name: "weight",
+            type: 'number',
+            placeholder: 'Weight (kg)',
+            min: 1,
+            max: 1000,
+          },
+          {
+            name: "gender",
+            placeholder: "Gender (m/f)"
+          },
+          {
+            name: "goal",
+            placeholder: "loss/gain/maintain"
+          },
+        ],
+      });
+      
+      await alert.present();
   }
 }
