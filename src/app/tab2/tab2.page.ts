5e6abe11246d45a5323b7f1f90f224dcaa0931fc
Johnson Dinh <jdinh782@gmail.com>
2023-03-09 14:55:30 -0800

    created page and content for recommendations

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { Tab2Service } from './tab2.service';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
 
-  constructor() {}
+
+export class Tab2Page implements OnInit {
+
+  constructor(private loadingCtrl: LoadingController, private navController: NavController) {}
+
+  ngOnInit(): void {}
+
+
+
+
+  // work on getting recommendations for different diets
+  // https://en.wikipedia.org/wiki/List_of_diets
+  async getRecommendations(searchQuery: Event) {
+
+  }
+
 
 }
