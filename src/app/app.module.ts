28a4c610cfa44ec6201a1f61081158ceb8762c38
Johnson Dinh <jdinh782@gmail.com>
2023-03-10 23:16:23 -0800

    diets now give recommendation results based on characteristics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [FormsModule,BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [FormsModule,BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, RouterModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -18,15 +18,22 @@ export class Tab2Page implements OnInit {
   // https://en.wikipedia.org/wiki/List_of_diets
   public diets = ["Low calorie diet", "Low carbohydrate diet", "Low fat diet", "Keto diet", "GERD diet"];
   public results = [...this.diets];
-  public foods: any = {};
+  public foods_data: any = {};
   public food_categories: Array<String> = [];
+  public recommended_results: any = {};
 
   constructor(private tab2Service: Tab2Service, private loadingCtrl: LoadingController, private navController: NavController) {}
 
   // load all data before any actions take place
-  ngOnInit(): void {
+  async ngOnInit() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading data...',
+      spinner: 'bubbles',
+    });
+    await loading.present();
+
     for (var i = 0; i < 5; i++) {
-      const foods_list = [];
+      // await this.sleep();
 
       this.tab2Service.getFoundationFoods(i+1).subscribe((res: any) => {
         const food_results = res['foods'];
@@ -40,38 +47,126 @@ export class Tab2Page implements OnInit {
           const foodCategory = food_results[key].foodCategory;
           const score = food_results[key].score;
 
-          const d = {id: id, name: name, publicationDate: publicationDate, nutrients: nutrients, foodCategory: foodCategory, score: score, sortHelper: -1};
+          const d = {id: id, name: name, publicationDate: publicationDate, nutrients: nutrients, foodCategory: foodCategory, score: score, sortHelper: 0};
           // console.log(d);
 
           if (!this.food_categories.includes(foodCategory)) {
             this.food_categories.push(foodCategory);
-            this.foods[foodCategory] = [d];
+            this.foods_data[foodCategory] = [d];
           } else {
-            this.foods[foodCategory].push(d);
+            this.foods_data[foodCategory].push(d);
           }
         })
       })
 
       // console.log('food_categories', this.food_categories)
     }
-    console.log("foods", this.foods);
+    loading.dismiss();
+    console.log("foods", this.foods_data);
   }
 
-  // work on getting recommendations for different diets
+  // nutritional amounts are based on every 100 grams of that food
   async getRecommendations(event: any) {
-    const chosenDiet = event.target.innerText
-    console.log(chosenDiet);
+    const chosen_diet = event.target.innerText
+    var diet_type = "low";
+    console.log(chosen_diet);
+
+    if (chosen_diet.toLowerCase().includes("low")) {
+      diet_type = "low";
+    } 
+    
+    if (chosen_diet.toLowerCase().includes("keto")) {
+      diet_type = "keto";
+    }
     
     const loading = await this.loadingCtrl.create({
-      message: 'Loading...',
+      message: 'Recommending foods...',
       spinner: 'bubbles',
     });
     await loading.present();
+    await this.sleep();
 
+    this.recommended_results = this.foods_data;
+
+    // sort each category by low/high diet (e.g. calorie, carbohydrate, etc.)
+    for (var foodCategory in this.foods_data) {
+      const foods_list = this.foods_data[foodCategory];
+
+      console.log(foodCategory, foods_list)
+
+      // low calorie diet (sort foods by calorie in ascending order)
+      for (var food in foods_list) {
+        const foodNutrients = foods_list[food].nutrients;
+        let nutrient_amount = 0;
+        let grams_amount = 100;
+  
+        for (var key in foodNutrients) {
+          // low calorie diet
+          if (chosen_diet.toLowerCase() === "low calorie diet") {
+            if (foodNutrients[key].unitName == 'KCAL') {
+              nutrient_amount = foodNutrients[key].value;
+              grams_amount = 100;
+  
+              break;
+            } 
+          }
+
+          // low carbohydrate diet
+          if (chosen_diet.toLowerCase() === "low carbohydrate diet") {
+            if (foodNutrients[key].nutrientName == 'Carbohydrate, by difference') {
+              nutrient_amount = foodNutrients[key].value;
+              grams_amount = 100;
+  
+              break;
+            }
+          }
+
+          // low fat and gerd diet
+          if (chosen_diet.toLowerCase() === "low fat diet" || chosen_diet.toLowerCase() === "gerd diet") {
+            if (foodNutrients[key].nutrientName == 'Total fat (NLEA)') {
+              nutrient_amount = foodNutrients[key].value;
+              grams_amount = 100;
+  
+              break;
+            }
+          }
+
+          // keto diet (high fat, moderate protein, very low carbs)
+          if (chosen_diet.toLowerCase() === "keto diet") {
+            if (foodNutrients[key].nutrientName == 'Total lipid (fat)') {
+              nutrient_amount = foodNutrients[key].value;
+              grams_amount = 100;
+  
+              break;
+            }
+          }
+        }
+
+        // console.log(`${foods_list[food].name} has ${nutrient_amount} ${foodNutrients[key].unitName} for every ${grams_amount} grams`);
+        this.recommended_results[foodCategory][food].sortHelper = nutrient_amount;
+        this.recommended_results[foodCategory][food]['servingAmount'] = grams_amount;
+      }
+
+      this.recommended_results[foodCategory].sort(function(a: any, b: any) {
+        if (diet_type === "low") {
+          return a.sortHelper - b.sortHelper
+        }
+
+        if (diet_type === "keto") {
+          // https://stackoverflow.com/questions/6913512/how-to-sort-an-array-of-objects-by-multiple-fields
+        }
+
+        return b.sortHelper - a.sortHelper;
+      })
+    }
+
+    console.log('recommended results', this.recommended_results);
 
     loading.dismiss();
-  };
 
+    // redirect to a separate page that will display the recommendation results
+    this.navController.navigateForward('/tabs/tab1')
+  };
 
   searchFilter(event: any) {
     const query = event.target.value.toLowerCase();
@@ -79,7 +174,7 @@ export class Tab2Page implements OnInit {
   }
 
   sleep() {
-    return new Promise(resolve => setTimeout(resolve, 5000));
+    return new Promise(resolve => setTimeout(resolve, 3000));
   }
 
 }
